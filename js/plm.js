fe9614d527162818649e693d4f5fd8e7f70da1c3
Partap Davis <partap@mail.com>
2009-12-20 15:58:21 -0700

    improve inline payout editor: update row checksum dynamically

--- js/plm.js
+++ js/plm.js
@@ -1,7 +1,33 @@
 $(document).ready(
     function() {
-	$('.edit').editable('/admin/plm/ajax/payout/save', {
-	    type   : 'text',
-	    loadurl: '/admin/plm/ajax/payout/load'
+
+	$('#payouts td.edit').editable('/admin/plm/ajax/payout/save', {
+	    indicator : '<img src="/' + Drupal.settings.plm.modulePath + '/img/indicator.gif">',
+	    loadurl   : '/admin/plm/ajax/payout/load',
+	    type      : 'text',
+	    callback : function(value, settings) {
+		//console.log('callback:');
+		//console.log('this:');
+		//console.log(this);
+		//console.log('value: ' + value.toString());
+		var row = $(this.parentNode);
+		var vals = row.children('td.rowval');
+		var pot = row.children('.rowtotal').html();
+		//var total = parseInt(value);
+		var total = 0;
+		for (var i=0;i<vals.length;i++) {
+		    //console.log(vals[i]);
+		    //console.log(vals[i].innerHTML);
+		    total += parseInt(vals[i].innerHTML);
+		}
+		var potRemaining = pot - total;
+		var rowchecksum = row.children('.rowchecksum');
+		if (potRemaining)
+		    rowchecksum.html(potRemaining);
+		else
+		    rowchecksum.html('');
+		//console.log('expected: ' + pot);
+		//console.log('total: ' + total);
+            }
 	});
     });
