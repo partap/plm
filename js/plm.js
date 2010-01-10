e28796052f748704fb501cfc5ae49c447ba33efd
Partap Davis <partap@mail.com>
2010-01-10 04:26:09 -0700

    fixed payout editor row - row checksumming - scroll on wide content

--- js/plm.js
+++ js/plm.js
@@ -1,37 +1,29 @@
 $(document).ready(
     function() {
 
-	$('#payouts td.edit').editable('/admin/plm/ajax/payout/save', {
+    $('#payouts td.edit').editable('/admin/plm/ajax/payout/save', {
         indicator : '<img src="/' + Drupal.settings.plm.modulePath + '/img/indicator.gif">',
-	    loadurl   : '/admin/plm/ajax/payout/load',
-	    type      : 'text',
-	    callback : function(value, settings) {
-	        //console.log('callback:');
-		    //console.log('this:');
-		    //console.log(this);
-		    //console.log('value: ' + value.toString());
-	        var row = $(this.parentNode);
-		    var vals = row.children('td.rowval');
-		    var pot = row.children('.rowtotal').html();
-		    //var total = parseInt(value);
-		    var total = 0;
-		    for (var i=0;i<vals.length;i++) {
-		        //console.log(vals[i]);
-		        //console.log(vals[i].innerHTML);
-		        total += parseInt(vals[i].innerHTML);
-		    }
-		    var potRemaining = pot - total;
-		    var rowchecksum = row.children('.rowchecksum');
-		    if (potRemaining)
-		        rowchecksum.html(potRemaining);
-	        else
-		        rowchecksum.html('');
-		    //console.log('expected: ' + pot);
-		    //console.log('total: ' + total);
+        loadurl   : '/admin/plm/ajax/payout/load',
+        type      : 'text',
+        width     : 'auto',
+        callback : function(value, settings) {
+            var row = $(this.parentNode);
+            var vals = row.children('td.rowval');
+            var pot = row.children('.rowtotal').html();
+            var total = 0;
+            for (var i=0;i<vals.length;i++) {
+                var n = parseInt(vals[i].innerHTML);
+                if (!isNaN(n))
+                    total += n;
+            }
+            var potRemaining = pot - total;
+            var rowchecksum = row.children('.rowchecksum');
+            if (potRemaining)
+                rowchecksum.html(potRemaining);
+            else
+                rowchecksum.html('');
         }
-	});
-
-	$('#plm-entrants-form');
+    });
 });
 
 function add_entrant(gid, pid) {
