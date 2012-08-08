bef3e7cba4d07fe3550a3a42102996bd8adc8c79
Partap Davis <partap@mail.com>
2012-08-08 02:39:56 -0600

    added jgrowl, results editor

--- js/plm.js
+++ js/plm.js
@@ -1,30 +1,56 @@
 $(document).ready(
     function() {
 
-    $('#payouts td.edit').editable(Drupal.settings.basePath+'admin/plm/ajax/payout/save', {
-        indicator : '<img src="/' + Drupal.settings.plm.modulePath + '/img/indicator.gif">',
-        loadurl   : Drupal.settings.basePath+'admin/plm/ajax/payout/load',
-        type      : 'text',
-        width     : 'auto',
-        callback : function(value, settings) {
-            var row = $(this.parentNode);
-            var vals = row.children('td.rowval');
-            var pot = row.children('.rowtotal').html();
-            var total = 0;
-            for (var i=0;i<vals.length;i++) {
-                var n = parseInt(vals[i].innerHTML);
-                if (!isNaN(n))
-                    total += n;
+        $('#payouts td.edit').editable(Drupal.settings.basePath+'admin/plm/ajax/payout/save', {
+            indicator : '<img src="/' + Drupal.settings.plm.modulePath + '/img/indicator.gif">',
+            loadurl   : Drupal.settings.basePath+'admin/plm/ajax/payout/load',
+            type      : 'text',
+            width     : 'auto',
+            callback : function(value, settings) {
+                var row = $(this.parentNode);
+                var vals = row.children('td.rowval');
+                var pot = row.children('.rowtotal').html();
+                var total = 0;
+                for (var i=0;i<vals.length;i++) {
+                    var n = parseInt(vals[i].innerHTML);
+                    if (!isNaN(n))
+                        total += n;
+                }
+                var potRemaining = pot - total;
+                var rowchecksum = row.children('.rowchecksum');
+                if (potRemaining)
+                    rowchecksum.html(potRemaining);
+                else
+                    rowchecksum.html('');
             }
-            var potRemaining = pot - total;
-            var rowchecksum = row.children('.rowchecksum');
-            if (potRemaining)
-                rowchecksum.html(potRemaining);
-            else
-                rowchecksum.html('');
-        }
-    });
-});
+        });
+        
+        $('#game-results .btn').live('click',function () {
+            var pid = this.id.split('-')[1];
+            var gid = $('#edit-gid').val();
+            var place = ($('#game-results tbody tr').index(($(this).closest('tr')))) + 1;
+            var url;
+            if ($(this).hasClass('player-done')) {
+                url = Drupal.settings.basePath + 'plm/ajax/game/player_done';
+            }
+            else if ($(this).hasClass('player-in')) {
+                url = Drupal.settings.basePath + 'plm/ajax/game/player_in';
+            }
+            //alert('Click! ' + url);
+            
+            $.post(url, {
+                gid: gid,
+                pid: pid,
+                place: place
+            },function (d, s, xhr) {
+                //$.jGrowl(d);
+                //alert(d);
+                $('#game-results').replaceWith(d);
+            });
+
+        });
+    }
+);
 
 function add_entrant(gid, pid) {
     var url = Drupal.settings.basePath+"plm/ajax/add_entrant";
